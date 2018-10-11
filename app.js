9d14624cb0722814ab384cddebac80792d986c07
italoandrade04 <italoandrade04@hotmail.com>
2018-10-10 21:54:43 -0300

    Correção da port do servidor (deploy heroku)

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ let route = require('./src/route/router');
 
 app.use('/api', route);
 
+let port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Servidor Rodando na porta 3000!');
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
 })
 
 module.exports = app;
